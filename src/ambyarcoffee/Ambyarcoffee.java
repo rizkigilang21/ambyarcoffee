13ad711e9c2336750abac97cd25c781719e1e451
rizkigilangRamadhan <rizkigilangramadhan@unud.ac.id>
2021-05-11 18:15:00 +0800

    firt commit

---
+++ src/ambyarcoffee/Ambyarcoffee.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ambyarcoffee;
+
+/**
+ *
+ * @author macbookair
+ */
+public class Ambyarcoffee {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
